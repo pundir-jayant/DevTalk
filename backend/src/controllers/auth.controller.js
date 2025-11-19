2d7b0baa0237d687fd731b277758daf102c7b067
pundir-jayant <jayantraj.pundir@gmail.com>
2025-11-19 10:43:24 +0530

    feat(auth): Add checkAuth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,8 +97,21 @@ export const updateProfile = async(req, res) => {
       return res.status(400).json({message:"Profile pic is required" });
     }
 
-    await cloudinary
+    const updateResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(userID, {profilePic:updateResponse.secure_url}, {new:true})
+
+    res.status(200).json(updateUser)
   } catch (error) {
-    
+    console.log("Error in update profile: ", error.message);
+    res.status(500).json({message:"Internal server error"});
+  }
+}
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller: ", error.message);
+    res.status(500).json({message:"Internal Server Error"});
   }
 }
