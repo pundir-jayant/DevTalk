4b6be860f724e224836280a98316ab055f892e08
pundir-jayant <jayantraj.pundir@gmail.com>
2025-11-26 11:57:28 +0530

    feat(messages): Add message schema and sidebar users endpoint

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async(req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUser =  await User.find({_id: {$ne:loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteredUser)
+    } catch (error) {
+        console.error("Error in getUsersForSlidebar: ", error.message);
+        res.status(500).json({message: "Internal server error" });
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import { connect_DB } from './lib/db.js';
+
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
