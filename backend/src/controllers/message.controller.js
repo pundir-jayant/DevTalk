7a8539bdab245691183c397722ca7d3a66126f26
pundir-jayant <jayantraj.pundir@gmail.com>
2025-11-30 12:48:16 +0530

    feat(messages): Add logic to fetch conversation history

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
 
 export const getUsersForSidebar = async(req, res) => {
     try {
@@ -9,5 +10,23 @@ export const getUsersForSidebar = async(req, res) => {
     } catch (error) {
         console.error("Error in getUsersForSlidebar: ", error.message);
         res.status(500).json({message: "Internal server error" });
+    }
+}
+
+export const getMessages = async(req, res) => {
+    try {
+        const { id:userToChatId } = req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        
     }
 }
