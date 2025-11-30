1665a8cd7e1c8a1fa566af324daeef69faffca91
pundir-jayant <jayantraj.pundir@gmail.com>
2025-11-30 17:13:13 +0530

    feat(messages): Implement sendMessage controller with image upload

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js"
 
-export const getUsersForSidebar = async(req, res) => {
+export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const filteredUser =  await User.find({_id: {$ne:loggedInUserId}}).select("-password");
@@ -13,7 +13,7 @@ export const getUsersForSidebar = async(req, res) => {
     }
 }
 
-export const getMessages = async(req, res) => {
+export const getMessages = async (req, res) => {
     try {
         const { id:userToChatId } = req.params
         const myId = req.user._id;
@@ -27,6 +27,37 @@ export const getMessages = async(req, res) => {
 
         res.status(200).json(messages)
     } catch (error) {
+        console.error("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });    
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
         
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await couldinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        //todo: realtime functionallity goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+         console.error("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });        
     }
 }
